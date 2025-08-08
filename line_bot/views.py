a35f976a85952b93c0bcd027adb010dca198fbc3
dsatoh20 <dsatoh20@gmail.com>
2025-08-08 17:32:58 +0900

    fixed response

--- line_bot/views.py
+++ line_bot/views.py
@@ -10,9 +10,15 @@ from line_bot.line_message import LineMessage
 def index(request):
     if request.method == 'POST':
         request = json.loads(request.body.decode('utf-8'))
+        if 'events' not in request or len(request['events']) == 0:
+            # LINE Developers検証用
+            return HttpResponse(status=200)
         data = request['events'][0]
         message = data['message']
         reply_token = data['replyToken']
         line_message = LineMessage(message_creater.create_single_text_message(message['text']))
         line_message.reply(reply_token)
-        return HttpResponse().status_code(200)
+
+        response = HttpResponse()
+        response.status_code = 200
+        return response
