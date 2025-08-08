3e1456f1d2905ef8e914373264aefbbeeec4d65e
dsatoh20 <dsatoh20@gmail.com>
2025-08-08 16:59:39 +0900

    added status code for response

--- line_bot/views.py
+++ line_bot/views.py
@@ -15,4 +15,4 @@ def index(request):
         reply_token = data['replyToken']
         line_message = LineMessage(message_creater.create_single_text_message(message['text']))
         line_message.reply(reply_token)
-        return HttpResponse("ok")
+        return HttpResponse().status_code(200)
