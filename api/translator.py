38e7e90862ff0735b82a144ac53bb61e45ca9c4f
dsatoh20 <dsatoh20@gmail.com>
2025-08-09 18:12:30 +0900

    added an instruction: to remain html element structure

--- api/translator.py
+++ api/translator.py
@@ -9,9 +9,10 @@ def translate_by_genai(textToTranslate):
         config=types.GenerateContentConfig(
             thinking_config=types.ThinkingConfig(thinking_budget=0), # Disables thinking
             system_instruction="You are a helpful assistant that translates text from Japanese to Osaka dialect. Please provide the translation in Osaka dialect. \
-                                \Fowllow the 2 instructions below.\
-                                \nInstruction1: If a message is osaka dialect, return as it is.\
-                                \nInstruction2: Exaggerate as osaka people do. (Most IMPORTANT)",
+                                \Fowllow the 3 instructions below.\
+                                \nInstruction1: You get a HTML element. Remain its structure as it is and translate just the text.\
+                                \nInstruction2: If a message is osaka dialect, return as it is.\
+                                \nInstruction3: Exaggerate as osaka people do. (Most IMPORTANT)",
         ),
     )
     messages = [
